010dbaf46d16e20fac1a1502374a015f36215547
Jorge Barcos <jorgebarcos88@gmail.com>
2019-07-31 06:11:19 -0300

    Estilos al contador

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	$('#fecha').countdown('2020/01/24', function(evento) {
-		$(this).html(evento.strftime('%D Dias &H Horas %M Minutos %S Segundos'));
+		$(this).html(
+			evento.strftime(
+				'<span> %D </span> Dias <span> &H </span> Horas <span> %M </span> Minutos <span> %S </span> Segundos'
+			)
+		);
 	});
 });
