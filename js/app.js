82c91839dc1070de6b6a47fc50ae1d82685fe7cb
Jorge Barcos <jorgebarcos88@gmail.com>
2019-07-31 06:45:44 -0300

    Agregar una animacion al hacer scroll en el sitio

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+// Cambia de color el fondo al hacer scroll
+window.onscroll = (e) => {
+	const scroll = window.scrollY;
+
+	const header = document.querySelector('#navegacion-principal');
+
+	if (scroll > 300) {
+		header.classList.add('bg-success');
+	} else {
+		header.classList.remove('bg-success');
+	}
+};
+
+// Jquery Countdown
 $(document).ready(function() {
 	$('#fecha').countdown('2020/01/24', function(evento) {
 		$(this).html(
